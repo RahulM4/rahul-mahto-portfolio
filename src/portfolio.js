f90ac2c4e98fcd85995d208c4b1313314dedd590
RAHUL MAHTO <75800571+RahulM4@users.noreply.github.com>
2023-08-08 15:06:14 +0530

    https://iamrahulmahto.wordpress.com/

--- src/portfolio.js
+++ src/portfolio.js
@@ -546,7 +546,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://blogs.rahulmahto.com/",
+    link: "https://iamrahulmahto.wordpress.com/",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
